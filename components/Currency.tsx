94569a1318e9da907b251b5ec850b1b245456e53
Mohammad Karimi <sw.karimi@gmail.com>
2024-06-01 16:08:58 +0330

    create currency IRR

---
+++ components/Currency.tsx
@@ -0,0 +1,16 @@
+import { formatPersianNumber } from "@/lib/format"
+import React, { FC } from "react"
+
+type CurrencyProps = {
+  amount: number
+  className?: string
+}
+
+export const Currency: FC<CurrencyProps> = ({ amount, className }) => {
+  return (
+    <div className={className}>
+      <span className="me-1">{formatPersianNumber(amount)}</span>
+      <span className="text-xs">تومان</span>
+    </div>
+  )
+}
