4dc402247a9ee5ebcf434bad9f42ead3d1cbd637
Mohammad Karimi <sw.karimi@gmail.com>
2024-06-02 14:53:15 +0330

    fix

--- hooks/useLocalStorage.ts
+++ hooks/useLocalStorage.ts
@@ -4,7 +4,8 @@ import { useEffect, useState } from "react"
 
 export function useLocalStorage<T>(key: string, initialValue: T | (() => T)) {
   const [value, setValue] = useState<T>(() => {
-    const jsonValue = typeof window !== 'undefined' ? localStorage.getItem(key) : ''
+    const jsonValue =
+      typeof window !== "undefined" ? localStorage.getItem(key) : null
     if (jsonValue != null) return JSON.parse(jsonValue)
     if (typeof initialValue === "function") {
       return (initialValue as () => T)()
