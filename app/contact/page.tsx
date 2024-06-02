eb2806ad68db896beba160d5091c05a4f03d36dd
Mohammad Karimi <sw.karimi@gmail.com>
2024-06-02 11:01:52 +0330

    comment

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,12 @@
 export default function ContactPage() {
   return (
-    <div>ContactPage</div>
+    <div className="flex flex-col items-center justify-center mt-20">
+      <div className="">تماس</div>
+      <div className="">هدف این تمرین، پیاده‌سازی سبد کالا می‌باشد</div>
+      <div className="">
+        به قسمت <strong>محصولات</strong> و یا <strong>سبد خرید </strong>مراجعه
+        نمایید و با انتخاب محصولات به چگونگی کارکرد آن توجه نمایید.
+      </div>
+    </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <div>MagazinePage</div>
+  return (
+    <div className="flex flex-col items-center justify-center mt-20">
+      <div className="">مجله</div>
+      <div className="">هدف این تمرین، پیاده‌سازی سبد کالا می‌باشد</div>
+      <div className="">
+        به قسمت <strong>محصولات</strong> و یا <strong>سبد خرید </strong>مراجعه
+        نمایید و با انتخاب محصولات به چگونگی کارکرد آن توجه نمایید.
+      </div>
+    </div>
+  )
 }
